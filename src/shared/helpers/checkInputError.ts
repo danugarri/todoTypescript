fc1ff62825b4474fc2403df43ae54bd637b9e0f9
danugarri <danugarri@hotmail.com>
2022-06-13 00:07:10 +0200

    fixed problem whrn clicking on add task button

--- src/shared/helpers/checkInputError.ts
+++ src/shared/helpers/checkInputError.ts
@@ -2,6 +2,8 @@ export 	const checkInput = (enteredText:string,addTodoHandler: Function): void =
         if(	enteredText.length > 0 ) {
             addTodoHandler(enteredText);
         }
+        else{
 
-       console.error("Introduce una tarea");
+            console.error("Introduce una tarea");
+        };
     };
