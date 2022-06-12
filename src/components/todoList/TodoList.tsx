aac5e94b40efca154c7b6917fbcc5293adcf0bbb
danugarri <danugarri@hotmail.com>
2022-06-12 22:19:15 +0200

    added close button

--- src/components/todoList/TodoList.tsx
+++ src/components/todoList/TodoList.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Todo } from '../../models/todos.model';
+import './TodoList.css';
 
 export const TodoItem: React.FC<{ item: Todo }> = (props) => {
 	const { item } = props;
 
-	return <li key={item.id}>{item.text}</li>;
+	return <div className='todo-list'><li key={item.id}>{item.text}</li><button className='close'>X</button></div>;
 };
