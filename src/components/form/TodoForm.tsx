875c2a2699cced12e5d1811295b7c15524911341
danugarri <danugarri@hotmail.com>
2022-06-13 01:45:30 +0200

    added reset input functionality

--- src/components/form/TodoForm.tsx
+++ src/components/form/TodoForm.tsx
@@ -1,19 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Todo } from '../../models/todos.model';
 import { checkInput } from '../../shared/helpers/checkInputError';
 
 export const TodoForm: React.FC<{ showTooltip: () => void, addTodoHandler:(todoText: string) => void, todos: Todo[] }> = (props) => {
 	const { showTooltip, addTodoHandler, todos } = props;
+	const [inputValue, setInputValue] = useState('');
+	const inputHandler = (e:any) =>{ 
+		setInputValue(e.target.value)
+		console.log(e.target.value)
+	}
 	//  Reference to the text input
 	const htmlInputText = useRef<HTMLInputElement>(null);			
 	//  onSubmit
 	const onSubmitHandler = (event: React.FormEvent): void => {
 		event.preventDefault();
 		const enteredText = htmlInputText.current!.value;
-	
-		
 		// checking input errors
 		checkInput({enteredText, addTodoHandler, todos});
+		// reset input value
+		setInputValue('');
 	};
 
 	return (
@@ -23,7 +28,7 @@ export const TodoForm: React.FC<{ showTooltip: () => void, addTodoHandler:(todoT
 					Añade una tarea
 				</label>
 				<br />
-				<input type='text' id='todo' ref= {htmlInputText} />
+				<input type='text' id='todo' ref= {htmlInputText} onChange= {inputHandler} value= {inputValue}/>
 				<button type='submit' >
 					Añadir
 				</button>
